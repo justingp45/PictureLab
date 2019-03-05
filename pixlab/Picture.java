1980169b7ccebb2879c718c01e2cf04d2d115e94
justingp45 <31289947+justingp45@users.noreply.github.com>
2019-03-05 12:27:04 -0800

    idk

--- pixlab/Picture.java
+++ pixlab/Picture.java
@@ -223,7 +223,7 @@ public class Picture extends SimplePicture
    */
   public static void main(String[] args) 
   {
-    Picture beach = new Picture("beach.jpg");
+    Picture beach = new Picture("pepe.jpg");
     beach.explore();
     beach.zeroBlue();
     beach.explore();
